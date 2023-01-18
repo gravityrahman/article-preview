c5f9a3adaff31a08d3bf641041a2132496aefae2
gravityrahman <105148637+gravityrahman@users.noreply.github.com>
2023-01-18 15:17:03 +0100

    Add files via upload

---
+++ article.js
@@ -0,0 +1,13 @@
+let shareBtnContainer = document.getElementById("share-container-one")   
+let shareBtn = document.getElementById("share-btn-div")
+
+
+
+shareBtn.addEventListener("click" ,function (){
+
+    shareBtnContainer.style.visibility = "visible";
+})
+
+shareBtnContainer.addEventListener("mouseleave", function (){
+    shareBtnContainer.style.visibility = "hidden";
+})
